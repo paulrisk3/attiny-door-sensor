78379d99d68eb85d0116710cec9e45e405cebf8c
Paul Risk <paulrisk3@gmail.com>
2021-03-21 22:18:34 -0400

    First sleep and wake. Needs testing.

--- attiny-door-sensor/attiny-door-sensor.ino
+++ attiny-door-sensor/attiny-door-sensor.ino
@@ -1,5 +1,22 @@
+#include <avr/sleep.h>
+
 const int led = 0;
 const int reedSwitch = 2;
+
+void going_to_sleep() {
+  sleep_enable();
+  attachInterrupt(reedSwitch, wake_up, LOW);
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  digitalWrite(led, LOW);
+  delay(1000);
+  sleep_cpu();
+  digitalWrite(led, HIGH);
+}
+
+void wake_up () {
+  sleep_disable();
+  detachInterrupt(reedSwitch);
+}
 
 void setup() {
   pinMode(led, OUTPUT);
